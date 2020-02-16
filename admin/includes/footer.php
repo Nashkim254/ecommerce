1fa0968492bceb1a0c495192c64d1f3a702a4ffd
programming6131 <kimutainashon@gmail.com>
2020-02-16 11:22:44 +0300

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2018 Brought To You By<a href="https://kimutainashon.000webhostapp.com/">Nash Freelancing</a></strong>
+</footer>
